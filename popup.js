cef2c26bf7611ad41e78888587e4edc60d2214a9
mahaalqerem <mahaalqerem1@gmail.com>
2023-07-08 14:45:01 +0300

    first commit

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Perform any actions when the DOM is loaded.
+  
+    // Example: Change the text color of the <h1> element.
+    var heading = document.querySelector('h1');
+    heading.style.color = 'red';
+  });
+  
